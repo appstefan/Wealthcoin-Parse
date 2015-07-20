93fa449822c04f1959691a643d8284c5d9806847
Stefan <stefan.britton@me.com>
2015-07-20 14:11:58 -0500

    switched back to block.io, withdrawal matching, ready for buy api calls

--- cloud/main.js
+++ cloud/main.js
@@ -5,100 +5,295 @@
 //https://9uH7wnXCApdzv4vtL38fN8w1F8YpWXzzqhx9ITtp:javascript-key%3DTi6naCnm6CVBjo5iAjljsV8ByduSBWnj2OSCF8ZL@api.parse.com/1/functions/ANY_FUNCTION
 
 //Make user a wallet.
-Parse.Cloud.afterSave("_User", function(request) {
+var blockApiKey = '6cc7-b07d-b22b-f6d2';
+
+Parse.Cloud.afterSave('_User', function(request) {
   if (!request.object.existed()) {
-    Parse.Cloud.useMasterKey();
-    var user = request.object;
-    user.set("balance", parseInt(0));
-    user.save();
-    //{"destination":"15qx9ug952GWGTNn7Uiv6vode4RcGrRemh","callback_url": "https://my.domain.com/callbacks/new-pay","token":"YOURTOKEN"}
-    var paymentForwardJson = {
-      'destination' : '2N2WUqQi3uAuFumfZynXSMozHDnCwCdvy7x',
-      'callback_url' : 'https://9uH7wnXCApdzv4vtL38fN8w1F8YpWXzzqhx9ITtp:javascript-key%3DTi6naCnm6CVBjo5iAjljsV8ByduSBWnj2OSCF8ZL@api.parse.com/1/functions/payment_received',
-      'token' : 'ba0c10cde294f1540ad5dbae854df7f9'
-    };
-    var paymentForwardJsonString = JSON.stringify(paymentForwardJson);
-
+    // Parse.Cloud.useMasterKey();
+    // var user = request.object;
+    // user.set('balance', parseInt(0));
+    // user.save();
     Parse.Cloud.httpRequest({
-      url: 'http://api.blockcypher.com/v1/btc/test3/payments',
-      method: 'POST',
-      body: paymentForwardJsonString,
-      success: function (httpResponse) {
-        console.log('json: ' + httpResponse.text);
-        var paymentForwardData = JSON.parse(httpResponse.text);
+      url: 'https://block.io/api/v2/get_new_address/?api_key='+blockApiKey,
+      success: function (response) {
+        var walletData = JSON.parse(response.text);
 
-        var PaymentForwardClass = Parse.Object.extend("PaymentForward");
-        var paymentForward = new PaymentForwardClass();
+        var network = walletData.data.network;
+        var userId = walletData.data.user_id;
+        var address = walletData.data.address;
+        var label = walletData.data.label;
+        var user = request.object;
 
-        var forwardId = paymentForwardData["id"];
-        paymentForward.set("inputAddress", paymentForwardData.input_address);
-        paymentForward.set("destination", paymentForwardData.destination);
-        paymentForward.set("forwardId", forwardId);
-        paymentForward.set("token", paymentForwardData.token);
-        paymentForward.set("user", request.object);
-        paymentForward.save();
+        var WalletClass = Parse.Object.extend('Wallet');
+        var wallet = new WalletClass();
 
-        console.log('payment forward created with input address: '+ paymentForwardData.input_address);
+        wallet.set("network", network);
+        wallet.set("userId", userId);
+        wallet.set("address", address);
+        wallet.set("label", label);
+        wallet.set("user", user);
+        //... add other wallet proerties
+        wallet.save();
+        console.log('created wallet: ' + address);
+
+        Parse.Cloud.httpRequest({
+          url: 'https://block.io/api/v2/create_notification/?api_key='+blockApiKey+'&type=address&address=' + walletData.data.address + '&url=https://9uH7wnXCApdzv4vtL38fN8w1F8YpWXzzqhx9ITtp:javascript-key%3DTi6naCnm6CVBjo5iAjljsV8ByduSBWnj2OSCF8ZL@api.parse.com/1/functions/transaction_received',
+          success: function (response) {
+            var notificationData = JSON.parse(response.text);
+
+            var network = notificationData.data.network;
+            var notificationId = notificationData.data.notification_id;
+            var type = notificationData.data.type;
+            var enabled = notificationData.data.enabled;
+            var url = notificationData.data.url;
+
+            var NotificationClass = Parse.Object.extend('Notification');
+            var notification = new NotificationClass();
+
+            notification.set('network', network);
+            notification.set('notificationId', notificationId);
+            notification.set('type', type);
+            notification.set('enabled', enabled);
+            notification.set('url', url);
+            notification.set('wallet', wallet);
+
+            notification.save();
+
+            console.log('notification created: ' + notificationId);
+          },
+          error: function (error) {
+            console.error('error creating notification' + error.text);
+          }
+        });
       },
-      error: function (httpResponse) {
-        console.error('error creating payment forward: ' + httpResponse);
+      error: function () {
+        console.error("wallet create failed");
       }
     });
   }
 });
 
-Parse.Cloud.define("payment_received", function(request, response) {
-    var paymentData = request.params;
-    console.log('payment being forwarded with value: ' + paymentData["value"]);
+Parse.Cloud.define("transaction_received", function(request, response) {
+  var transactionData = request.params;
+  if (transactionData.data) {
+    var notificationId = transactionData.notification_id;
+    var deliveryAttempt = transactionData.delivery_attempt;
+    var confirmations = transactionData.data.confirmations;
+    var address = transactionData.data.address;
+    var amountReceived = transactionData.data.amount_received;
+    var amountSent = transactionData.data.amount_sent;
+    var balanceChange = transactionData.data.balance_change;
+    var network = transactionData.data.network;
+    var transactionId = transactionData.data.txid;
+    var sentAt = transactionData.created_at;
 
-    var PaymentClass = Parse.Object.extend("Payment");
-    var payment = new PaymentClass();
+    console.log("transaction received: " + notificationId);
+    console.log("confirmations: " + confirmations);
 
-    var value = paymentData["value"];
-    var destination = paymentData["destination"];
-    var input_address = paymentData["input_address"];
-    var input_transaction_hash = paymentData["input_transaction_hash"];
-    var transaction_hash = paymentData["transaction_hash"];
+    var TransactionClass = Parse.Object.extend('Transaction');
+    var query = new Parse.Query(TransactionClass);
+    query.equalTo('transactionId', transactionId);
+    query.find({
+      success: function(results) {
+        if(results.length === 0) {
+          console.log('making new transaction...');
+          var transaction = new TransactionClass();
 
-    payment.set("value", value);
-    payment.set("destination", destination);
-    payment.set("inputAddress", input_address);
-    payment.set("inputTransactionHash", input_transaction_hash);
-    payment.set("transactionHash", transaction_hash);
-    payment.save();
+          transaction.set('notificationId', notificationId);
+          transaction.set('deliveryAttempt', deliveryAttempt);
+          transaction.set('confirmations', confirmations);
+          transaction.set('address', address);
+          transaction.set('amountReceived', amountReceived);
+          transaction.set('amountSent', amountSent);
+          transaction.set('balanceChange', balanceChange);
+          transaction.set('network', network);
+          transaction.set('transactionId', transactionId);
+          transaction.set('sentAt', sentAt);
 
-    console.log('payment saved');
+          transaction.save();
+        } else {
+          console.log('updating existing transaction...');
+          var transaction = results[0];
+          transaction.set("confirmations", confirmations);
+          transaction.save();
+        }
+        response.success();
+      },
+      error: function(error) {
+        alert("Error: " + error.code + " " + error.message);
+        response.success();
+      }
+    });
+  } else {
+    console.log('ping received');
     response.success();
+  };
 });
 
-Parse.Cloud.afterSave("Payment", function(request) {
-  Parse.Cloud.useMasterKey();
-  var inputAddress = request.object.get("inputAddress");
-  var value = request.object.get("value");
+Parse.Cloud.afterSave("Transaction", function(request) {
+  var transaction = request.object;
+  var confirmations = transaction.get('confirmations');
+  var notificationId = transaction.get('notificationId');
+  var deliveryAttempt = transaction.get('deliveryAttempt');
+  var address = transaction.get('address');
+  var amountReceived = transaction.get('amountReceived');
+  var balanceChange = transaction.get('balanceChange');
+  var sentAt = transaction.get('sentAt');
 
-  console.log('searching for matching paymentForward for input address: ' + inputAddress);
+  console.log('transaction saved with balanceChange: ' + balanceChange);
 
-  var paymentForward = Parse.Object.extend("PaymentForward");
-  var query = new Parse.Query(paymentForward);
-  query.equalTo("inputAddress", inputAddress);
-  query.include("user")
-  query.find({
-    success: function(results) {
-      var object = results[0];
-      var user = object.get("user");
+  if (confirmations === 3) {
+    if (balanceChange > 0) {
+      console.log('forwarding to 1broker...');
+      Parse.Cloud.httpRequest({
+        method: 'GET',
+        url: 'https://1broker.com/api/v1/account/bitcoindepositaddress.php?token=24727a21f9effcdce3363a712cfc1f66&pretty=1',
+        success: function (addressResponse) {
+          var newBrokerAddress = addressResponse.data.response.bitcoin_address;
+          console.log('current broker address: ' + newBrokerAddress);
+          var BrokerClass = Parse.Object.extend("Broker");
+          var query = new Parse.Query(BrokerClass);
+          query.first({
+            success: function(object) {
+              var broker = object;
+              var currentBrokerAddress = object.get("address");
+              if (currentBrokerAddress != newBrokerAddress) {
+                console.log('broker address: ' + currentBrokerAddress + ' is deprecated. replacing with: ' + newBrokerAddress);
+                broker.set("address", newBrokerAddress);
+                broker.save();
+              }
+              Parse.Cloud.httpRequest({
+                method: 'POST',
+                url: 'https://block.io/api/v2/get_network_fee_estimate/?api_key='+blockApiKey+'&from_addresses='+ address +'&to_addresses='+ newBrokerAddress +'&amounts='+ balanceChange +'&pin=wealthcoin',
+                success: function (response) {
+                  var estimateData = JSON.parse(response.text);
+                  var estimatedFee = estimateData.data.estimated_network_fee;
+                  var withdrawAmount = (((balanceChange * 1000000)-(estimatedFee * 1000000)) / 1000000);
+                  console.log('fee estimate: ' + estimatedFee + '| withdrawing: ' + withdrawAmount);
+                  Parse.Cloud.httpRequest({
+                    method: 'POST',
+                    url: 'https://block.io/api/v2/withdraw_from_addresses/?api_key='+blockApiKey+'&from_addresses='+ address +'&to_addresses='+ newBrokerAddress +'&amounts='+ withdrawAmount +'&pin=wealthcoin',
+                    success: function (response) {
+                      var withdrawalData = JSON.parse(response.text);
 
-      var currentBalance = parseInt(user.get("balance"));
-      var newBalance = parseInt(currentBalance + value);
-      console.log('new balance: ' + newBalance);
-      user.set("balance", newBalance);
-      user.save();
+                      var status = withdrawalData.status;
+                      var network = withdrawalData.data.network;
+                      var transactionId = withdrawalData.data.txid;
+                      var amountWithdrawn = withdrawalData.data.amount_withdrawn;
+                      var amountSent = withdrawalData.data.amount_sent;
+                      var networkFee = withdrawalData.data.network_fee;
+                      var blockioFee = withdrawalData.data.blockio_fee;
+
+                      var WithdrawalClass = Parse.Object.extend('Withdrawal');
+                      var withdrawal = new WithdrawalClass();
+                      withdrawal.set('status', status);
+                      withdrawal.set('network', network);
+                      withdrawal.set('transactionId', transactionId);
+                      withdrawal.set('amountWithdrawn', amountWithdrawn);
+                      withdrawal.set('amountSent', amountSent);
+                      withdrawal.set('networkFee', networkFee);
+                      withdrawal.set('blockioFee', blockioFee);
+                      withdrawal.save();
+                      console.log('withdrawal created: ' + JSON.stringify(withdrawalData));
+                    },
+                    error: function (error) {
+                      console.error('error creating withdrawal' + error.text);
+                    }
+                  });
+                },
+                error: function (error) {
+                  console.error('error creating withdrawal' + error.text);
+                }
+              });
+            },
+            error: function(error) {
+              alert("Error: " + error.code + " " + error.message);
+            }
+          });
+        },
+        error: function (error) {
+          alert("Error: " + error.code + " " + error.message);
+        }
+      });
+    } else {
+      //moved here to avoid var duplication with withdrawal
+      var amountSent = transaction.get('amountSent');
+      var network = transaction.get('network');
+      var transactionId = transaction.get('transactionId');
+
+      var WithdrawalClass = Parse.Object.extend("Withdrawal");
+      var query = new Parse.Query(WithdrawalClass);
+      query.equalTo('transactionId', transactionId);
+      query.first({
+        success: function(withdrawalObject) {
+          console.log('buying from 1broker...');
+
+          var WalletClass = Parse.Object.extend("Wallet");
+          var query = new Parse.Query(WalletClass);
+          query.equalTo("address", address);
+          query.include("user");
+          query.find({
+            success: function(results) {
+              var object = results[0];
+              var user = object.get("user");
+
+              var portfolioPointer = user.get("portfolio");
+
+              portfolioPointer.fetch({
+                success: function(object) {
+                  var portfolio = object;
+                  var stockRatio = portfolio.get("stockRatio");
+                  // var stockRatio = portfolio.get("stockRatio");
+                  // var stockRatio = portfolio.get("stockRatio");
+                  console.log('user: ' + user.get('username') + 'stockRatio: ' + stockRatio);
+                  // var buyRequest = {
+                  //   'stockRatio' : stockRatio,
+                  //   'total' : total
+                  // };
+                  // Parse.Cloud.run('perform_broker_buy', buyRequest, {
+                  //   success: function() {
+                  //
+                  //   },
+                  //   error: function(error) {
+                  //
+                  //   }
+                  // });
+                },
+                error: function(object, error) {
+                  console.console.error('error fetching porfolio: ' + error.message);
+                }
+              });
+            },
+            error: function(error) {
+              alert("Error querying wallet: " + error.code + " " + error.message);
+            }
+          });
+        },
+        error: function(error) {
+          console.log('no matching withdrawal found with txID: ' + transactionId);
+        }
+      });
+    }
+  }
+});
+
+Parse.Cloud.define("perform_broker_buy", function(request, response) {
+  var stockMargin = ((request.params.total * request.params.stockRatio)/1000000);
+  console.log('stockMargin: ' + stockMargin);
+  // response.success();
+
+  Parse.Cloud.httpRequest({
+    url: 'https://1broker.com/api/v1/order/create.php?symbol=SP500&margin=' + stockMargin + '&direction=long&leverage=1&order_type=Market&token=24727a21f9effcdce3363a712cfc1f66&pretty=1',
+    success: function (d) {
+      console.log('buy success');
+      response.success(d.params);
     },
-    error: function(error) {
-      alert("Error: " + error.code + " " + error.message);
+    error: function (d) {
+      console.log('buy error');
+      response.error(d.params);
     }
   });
 });
-
 
 //keep around for reference future use
 Parse.Cloud.define("get_broker_balance", function(request, response) {
